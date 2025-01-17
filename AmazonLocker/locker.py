54e2609837659454347202ae4c0d522de214186c
vedantspatil <37808420+vedantspatil@users.noreply.github.com>
2025-01-17 14:36:25 -0800

    chatgpt refactored

--- AmazonLocker/locker.py
+++ AmazonLocker/locker.py
@@ -6,136 +6,113 @@ class Size(Enum):
     MEDIUM = 2
     LARGE = 3
 
-class User(ABC):
+class User:
     def __init__(self, id, name, code):
         self.id = id
         self.name = name
         self.code = code
-    
-    def validateCode(self, code):
+
+    def validate_code(self, code):
         return self.code == code
 
 class DeliveryUser(User):
-    def __init__(self, id, name, code):
-        super().__init__(id, name, code)
+    pass
 
 class EndUser(User):
-    def __init__(self, id, name, code):
-        super().__init__(id, name, code)
-    
+    pass
 
-class Package(ABC):
+class Package:
     def __init__(self, id, size, user):
         self.id = id
         self.size = size
         self.user = user
 
-class LargePackage(Package):
-    def __init__(self, id, user):
-        super().__init__(id, Size.LARGE, user)
-    
-class MediumPackage(Package):
-    def __init__(self, id, user):
-        super().__init__(id, Size.MEDIUM, user)
-        
-class SmallPackage(Package):
-    def __init__(self, id, user):
-        super().__init__(id, Size.SMALL, user)
-
-
 class Locker(ABC):
     def __init__(self, id, size):
         self.id = id
-        self.code = None
         self.size = size
-        self.isAvailable = True
+        self.is_available = True
         self.package = None
-    
-    def assignPackage(self, package):
-        if self.isAvailable:
-            self.isAvailable = False
+        self.code = None
+
+    def assign_package(self, package):
+        if self.is_available:
+            self.is_available = False
             self.package = package
-            self._assignCode(self.package.user.code)
-    
-    def emptyLocker(self):
-        self.isAvailable = True
+            self.code = package.user.code
+
+    def empty_locker(self):
+        self.is_available = True
         self.package = None
-        self._assignCode(None)
-    
-    def validateCode(self, code):
-        return code == self.code
+        self.code = None
 
-    def _assignCode(self, code):
-        self.code = code
+    def validate_code(self, code):
+        return self.code == code
 
-    def open(self):
-        return self.emptyLocker()
-    
+    def open_locker(self):
+        self.empty_locker()
+
     @abstractmethod
-    def canFit(self, package):
+    def can_fit(self, package):
         pass
 
 class SmallLocker(Locker):
     def __init__(self, id):
         super().__init__(id, Size.SMALL)
 
-    def canFit(self, package):
+    def can_fit(self, package):
         return package.size == Size.SMALL
 
 class MediumLocker(Locker):
     def __init__(self, id):
         super().__init__(id, Size.MEDIUM)
 
-    def canFit(self, package):
+    def can_fit(self, package):
         return package.size in [Size.MEDIUM, Size.SMALL]
 
 class LargeLocker(Locker):
     def __init__(self, id):
         super().__init__(id, Size.LARGE)
 
-    def canFit(self, package):
-        return package.size == Size.LARGE
-
+    def can_fit(self, package):
+        return package.size in [Size.LARGE, Size.MEDIUM, Size.SMALL]
 
 class LockerSystem:
     def __init__(self):
         self.lockers = []
-        self.userPackages = {}
-        self.packageLocation = {}
+        self.user_packages = {}
+        self.package_location = {}
 
-    def addLockers(self, locker):
+    def add_locker(self, locker):
         self.lockers.append(locker)
-    
-    def _findLocker(self, package):
+
+    def _find_locker(self, package):
         currLocker = None
         for locker in self.lockers:
-            if locker.isAvailable and locker.canFit(package):
+            if locker.is_available and locker.can_fit(package):
                 if not currLocker or locker.size <= currLocker.size:
                     currLocker = locker
         return currLocker
 
-    def assignLocker(self, user, package):
+    def assign_locker(self, user, package):
         if isinstance(user, DeliveryUser):
-            locker = self._findLocker(package)
+            locker = self._find_locker(package)
             if locker:
-                locker.assignPackage(package)
-                self.packageLocation[package.id] = locker
-                self.userPackages[package.user.id] = package
-                return "Package Assigned to Locker {0}".format(locker.id)
-            else:
-                return "No Locker Found for the Package"
+                locker.assign_package(package)
+                self.package_location[package.id] = locker
+                self.user_packages[package.user.id] = package
+                return f"Package Assigned to Locker {locker.id}"
+            return "No Locker Found for the Package"
         return "Only valid Delivery Agents can deliver packages"
 
-
-    def pickUpPackage(self, user):
-        if isinstance(user, EndUser) and user.id in self.userPackages:
-            package = self.userPackages[user.id]
-            locker = self.packageLocation[package.id]
-            if locker.validateCode(user.code):
-                locker.open()
-                del self.userPackages[user.id]
-                del self.packageLocation[package.id]
+    def pick_up_package(self, user):
+        if isinstance(user, EndUser) and user.id in self.user_packages:
+            package = self.user_packages[user.id]
+            locker = self.package_location[package.id]
+            if locker.validate_code(user.code):
+                locker.open_locker()
+                del self.user_packages[user.id]
+                del self.package_location[package.id]
                 return "Package Picked Up"
-            else:
-                return "Invalid Code"
+            return "Invalid Code"
         return "No Package for User"
